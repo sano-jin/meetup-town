b2f8d75af34511c89b942593b572c1377a664b6f
sano-jin <sano-jin@akane.waseda.jp>
2021-04-17 20:53:21 +0900

    let index.js to typescript

--- index.js
+++ index.js
@@ -1,80 +1,73 @@
-'use strict';
-
-//Loading dependencies & initializing express
-const os = require('os');
-const express = require('express');
-const app = express();
-const http = require('http');
-//For signalling in WebRTC
-const socketIO = require('socket.io');
-
-
-app.use(express.static('public'))
-
-app.get("/", (req, res) => {
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
+exports.__esModule = true;
+var os_1 = require("os");
+var express = require("express");
+var http_1 = require("http");
+var socket = require("socket.io");
+// For signalling in WebRTC
+// initializing express
+var app = express();
+app.use(express.static('public'));
+app.get("/", function (req, res) {
     res.render("index.ejs");
 });
-
-const server = http.createServer(app);
-
+var server = http_1.createServer(app);
 server.listen(process.env.PORT || 8000);
-
-const io = socketIO(server);
-
-io.sockets.on('connection', (socket) => {
-
+var io = socket(server);
+io.sockets.on('connection', function (socket) {
     // Convenience  to log server messages on the client.
     // Arguments is an array like object which contains all the arguments of log(). 
-    // To push all the arguments of log() in array, we have to use apply().
-    const log = (...param) => {
-        const array = ['Message from server:', ...param];
+    var log = function () {
+        var param = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param[_i] = arguments[_i];
+        }
+        var array = __spreadArray(['Message from server:'], param);
         socket.emit('log', array);
     };
-
-
-    //Defining Socket Connections
-    socket.on('message', (message, room) => {
+    // Defining Socket Connections
+    socket.on('message', function (message, room) {
         log('Client said: ', message);
         // for a real app, would be room-only (not broadcast)
-        socket.in(room).emit('message', message, room);
+        socket["in"](room).emit('message', message, room);
     });
-
-    socket.on('create or join', (room) => {
-        log(`Received request to create or join room ${room}`);
-
-        const clientsInRoom = io.sockets.adapter.rooms[room];
-        const numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-        log(`Room ${room} now has ${numClients} client(s)`);
-
+    socket.on('create or join', function (room) {
+        log("Received request to create or join room " + room);
+        var clientsInRoom = io.sockets.adapter.rooms[room];
+        var numClients = clientsInRoom ? Object.keys(clientsInRoom).length : 0;
+        log("Room " + room + " now has " + numClients + " client(s)");
         if (numClients === 0) {
             socket.join(room);
-            log(`Client ID ${socket.id} created room ${room}`);
+            log("Client ID " + socket.id + " created room " + room);
             socket.emit('created', room, socket.id);
-
-        } else if (numClients === 1) {
-            log(`Client ID ${socket.id} joined room ${room}`);
-            io.sockets.in(room).emit('join', room);
+        }
+        else if (numClients === 1) {
+            log("Client ID " + socket.id + " joined room " + room);
+            io.sockets["in"](room).emit('join', room);
             socket.join(room);
             socket.emit('joined', room, socket.id);
-            io.sockets.in(room).emit('ready');
-        } else { // max two clients
+            io.sockets["in"](room).emit('ready');
+        }
+        else { // max two clients
             socket.emit('full', room);
         }
     });
-
-    socket.on('ipaddr', () => {
-        const ifaces = os.networkInterfaces();
-        for (const dev of ifaces) {
-            dev.forEach((details) => {
+    socket.on('ipaddr', function () {
+        var ifaces = os_1.networkInterfaces();
+        for (var dev in ifaces) {
+            ifaces[dev].forEach(function (details) {
                 if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
                     socket.emit('ipaddr', details.address);
                 }
             });
         }
     });
-
-    socket.on('bye', () => {
+    socket.on('bye', function () {
         console.log('received bye');
     });
-
 });
