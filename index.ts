59b535249f0e04743cd575406e1ac4418a1eb7ff
sano-jin <sano@ueda.info.waseda.ac.jp>
2021-04-17 20:53:21 +0900

    let index.js to typescript

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
-'use strict';
+import { networkInterfaces } from 'os';
+import * as express from 'express';
+import { createServer, Server as httpServer } from 'http';
+import * as socket from 'socket.io';
+// For signalling in WebRTC
 
-//Loading dependencies & initializing express
-const os = require('os');
-const express = require('express');
-const app = express();
-const http = require('http');
-//For signalling in WebRTC
-const socketIO = require('socket.io');
-
+// initializing express
+const app: express.Express = express();
 
 app.use(express.static('public'))
 
@@ -15,35 +13,35 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-const server = http.createServer(app);
+const server: httpServer = createServer(app);
 
 server.listen(process.env.PORT || 8000);
 
-const io = socketIO(server);
+const io: socket.Server = socket(server);
 
-io.sockets.on('connection', (socket) => {
+io.sockets.on('connection', (socket: socket.Socket) : void => {
 
     // Convenience  to log server messages on the client.
     // Arguments is an array like object which contains all the arguments of log(). 
-    // To push all the arguments of log() in array, we have to use apply().
-    const log = (...param) => {
+    const log = (...param) : void => {
         const array = ['Message from server:', ...param];
         socket.emit('log', array);
     };
 
 
-    //Defining Socket Connections
-    socket.on('message', (message, room) => {
+    // Defining Socket Connections
+    socket.on('message', (message: string, room: string) : void => {
         log('Client said: ', message);
         // for a real app, would be room-only (not broadcast)
         socket.in(room).emit('message', message, room);
     });
 
-    socket.on('create or join', (room) => {
+    socket.on('create or join', (room: string) => {
         log(`Received request to create or join room ${room}`);
 
-        const clientsInRoom = io.sockets.adapter.rooms[room];
-        const numClients = clientsInRoom ? Object.keys(clientsInRoom).length : 0;
+        const clientsInRoom: Set<socket.id> = io.sockets.adapter.rooms[room];
+        const numClients : number =
+            clientsInRoom ? Object.keys(clientsInRoom).length : 0;
         log(`Room ${room} now has ${numClients} client(s)`);
 
         if (numClients === 0) {
@@ -62,18 +60,18 @@ io.sockets.on('connection', (socket) => {
         }
     });
 
-    socket.on('ipaddr', () => {
-        const ifaces = os.networkInterfaces();
-        for (const dev of ifaces) {
-            dev.forEach((details) => {
+    socket.on('ipaddr', () : void => {
+        const ifaces = networkInterfaces();
+        for (const dev in ifaces) {
+            ifaces[dev].forEach((details) => {
                 if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
                     socket.emit('ipaddr', details.address);
                 }
             });
         }
-    });
+});
 
-    socket.on('bye', () => {
+    socket.on('bye', () : void => {
         console.log('received bye');
     });
 
