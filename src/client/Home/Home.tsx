f4d21ed420131ad9b025f847bdf3afbdec7e2575
sano-jin <sano-jin@akane.waseda.jp>
2021-08-02 23:29:55 +0900

    updated

--- src/client/Home/Home.tsx
+++ src/client/Home/Home.tsx
@@ -24,7 +24,6 @@ class Home extends React.Component<{}, {}> {
 	    <Grid container justify="center" >
 		<Box display="flex" alignItems="center" height="100vh">
 		    <Button
-			className="createNewRoom"
 			component={Link}
 			to={`/rooms/${uuid()}`}
 			color="primary"
